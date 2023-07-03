b354d85b0c8eb9952916d98463aa5b6cde4c7cf6
luisa <luisa.oliveira.vicente@hotmail.com>
2023-07-03 16:58:37 -0300

    Rotas e pacotes

--- porco-rosso/src/App.jsx
+++ porco-rosso/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage/HomePage";
+import { Login } from "./Login/Login";
+import { Cadastrar } from "./Cadastrar/Cadastrar";
+import { SobreNos } from "./SobreNos/SobreNos";
 
 const App = () => {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/Login/Login" element={<Login />} />
+        <Route exact path="/Cadastrar/Cadastrar" element={<Cadastrar />} />
+        <Route exact path="/SobreNos/SobreNos" element={<SobreNos />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
